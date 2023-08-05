7061918d6096638a4be6d8c8cdcec884d6191b9f
WXXHHZ <WXXHHZ@gmail.com>
2023-08-05 21:24:15 +0800

    初学c语言

---
+++ tese_1.c
@@ -0,0 +1,102 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{ //选择语句
+//	int input = 0;
+//	scanf("%d", &input);
+//	if (input == 1)//if语句后不加；
+//	{
+//		printf("%d", 1);
+//
+//	}
+//	else
+//	{
+//		printf("%d", 0);
+//	}
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	//while循环也不加；//while与if的结合使用
+//	int line = 0;
+//	while (line< 100)//似乎这类都不加；与"
+//	{
+//
+//		printf("写代码%d\n", line);
+//		line++;
+//		if (line<50)
+//		{
+//			printf("%d\n", 1);
+//		}
+//		else
+//		{
+//			printf("%d\n", 0);
+//		}
+//	
+//	}
+//	///
+//	if (line = 100)
+//	{
+//		printf("%d\n", 1);
+//
+//	}
+//	
+//
+//
+//
+//	return 0;
+//}
+
+////利用函数求两个数相加
+//int add(int x,int  y)//写函数时注意return，要有一个返回的值（用来防止代码冗余）
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d",&num1 ,& num2);
+//	int sum = add(num1, num2);
+//	printf("%d\n", sum);
+//
+//
+//
+//	return 0;
+//
+
+
+
+//
+////数组-一组相同类型元素的集合
+////数组使用下标访问的，下标是从0开始的
+//int main()
+//{
+//	int szm[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i;
+//	scanf("%d", &i);
+//	while (i < 10)
+//	{
+//		printf("%d ", szm[i]);
+//		i++;
+//	}
+//	//char ch ={'a','b','c'}//不完全初始化，剩余的默认为0！！！！！
+//	return 0;
+//}
+
+
+
+
+int main()
+{
+
+
+
+
+	return 0;
+}
